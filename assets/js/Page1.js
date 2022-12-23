22564736fed8cf2091a08f273652f292b5030de5
IlBennys <119436512+IlBennys@users.noreply.github.com>
2022-12-23 12:17:02 +0100

    okok

---
+++ assets/js/Page1.js
@@ -0,0 +1,11 @@
+const attenzione = (e) => {
+  let box = document.querySelector(".checkbox__input")
+  if (box.checked === false) {
+    alert("WARNING! ACCEPT THE TERMS & CONDITIONS TO CONTINUE!")
+  } else {
+    window.location.href = "./questionPage.html"
+  }
+}
+const fatto = (e) => {
+  document.getElementById("ester_egg").style.opacity = "100"
+}
